b87405c5a07f903f8410b2b37003a2a17df44386
Sandile Ngubeni <Sandile.Ngubeni@multichoice.co.za>
2021-04-09 06:35:59 +0200

    adds the student record

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { LoginPage } from '../pages/login/login';
-import { SignupPage } from '../pages/signup/signup';
 import { AttendancePage } from '../pages/attendance/attendance';
 import { OnePage } from '../pages/one/one';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = OnePage;
+  rootPage:any = LoginPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
